0fd0f7320e7f4523eb105faf485ca68ae3070397
Soham02 <soham.mhatre97@gmail.com.com>
2020-05-01 18:51:01 -0400

    Base Program

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ChocoAn
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTextBoxIdNumber = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroButtonLogin = new MetroFramework.Controls.MetroButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroTextBoxIdNumber
+            // 
+            this.metroTextBoxIdNumber.Location = new System.Drawing.Point(113, 211);
+            this.metroTextBoxIdNumber.Name = "metroTextBoxIdNumber";
+            this.metroTextBoxIdNumber.Size = new System.Drawing.Size(251, 23);
+            this.metroTextBoxIdNumber.TabIndex = 1;
+            this.metroTextBoxIdNumber.Click += new System.EventHandler(this.metroTextBox1_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(113, 179);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(108, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Enter ID Number";
+            this.metroLabel1.Click += new System.EventHandler(this.metroLabel1_Click);
+            // 
+            // metroButtonLogin
+            // 
+            this.metroButtonLogin.Location = new System.Drawing.Point(289, 265);
+            this.metroButtonLogin.Name = "metroButtonLogin";
+            this.metroButtonLogin.Size = new System.Drawing.Size(75, 23);
+            this.metroButtonLogin.TabIndex = 4;
+            this.metroButtonLogin.Text = "Login";
+            this.metroButtonLogin.Click += new System.EventHandler(this.metroButtonLogin_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ChocoAn.Properties.Resources.unnamed;
+            this.pictureBox1.Location = new System.Drawing.Point(113, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(251, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(485, 450);
+            this.Controls.Add(this.metroButtonLogin);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroTextBoxIdNumber);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBoxIdNumber;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton metroButtonLogin;
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChocoAn
+{
+    public partial class LoginForm : Form
+    {
+        public bool loginFlag { get; set; }
+        public LoginForm()
+        {
+            InitializeComponent();
+            loginFlag = false;
+        }
+
+        private void metroTextBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButtonLogin_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.IdNumbersTableAdapter IDAda = new DataSet1TableAdapters.IdNumbersTableAdapter();
+            DataTable dt = IDAda .GetDataByIDNumber (metroTextBoxIdNumber.Text);
+
+            if(dt.Rows.Count > 0)
+            {
+                //valid
+                MessageBox.Show("Login Successful");
+                loginFlag = true;
+
+
+            }
+            else
+            {
+                //not valid
+                MessageBox.Show("Login Failed");
+                loginFlag = false;
+                
+            }
+
+            Close();
+
+        }
+
+        
+    }
+}
